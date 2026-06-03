3aab904f6a7b812c161906546a30f1ecf2727aec
AteszXD <keria@kkszki.hu>
2026-06-03 19:05:56 +0200

    gunshot + sfx

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -3,25 +3,31 @@ using UnityEngine;
 public class Bullet : MonoBehaviour
 {
     public float damage = 25f;
+    public AudioClip shootSound;
+    private AudioSource audioSource;
+
+    private void Awake()
+    {
+        audioSource = GetComponent<AudioSource>();
+        if (shootSound != null) audioSource.PlayOneShot(shootSound);
+    }
 
     private void OnCollisionEnter(Collision collision)
     {
-		Target target = collision.gameObject.GetComponent<Target>();
-		if (target != null)
-		{
-			target.TakeDamage(damage);
-			Destroy(gameObject);
-			return;
-		}
+        Target target = collision.gameObject.GetComponent<Target>();
+        if (target != null)
+        {
+            target.TakeDamage(damage);
+            Destroy(gameObject);
+            return;
+        }
 
-		PlayerHealth player = collision.gameObject.GetComponent<PlayerHealth>();
-		if (player != null)
-		{
-			player.TakeDamage(damage);
-			Destroy(gameObject);
-			return;
-		}
-
-		// Destroy(gameObject); // hit a wall or something else
+        PlayerHealth player = collision.gameObject.GetComponent<PlayerHealth>();
+        if (player != null)
+        {
+            player.TakeDamage(damage);
+            Destroy(gameObject);
+            return;
+        }
     }
 }
